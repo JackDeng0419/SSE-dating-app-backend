3c911c9ffe25cf8402022bb147d554eed155ea4f
JackNeedFocus <Jack.z.deng@outlook.com>
2022-10-02 10:35:15 +1030

    change the Chinese comments

--- app/service/home.js
+++ app/service/home.js
@@ -6,9 +6,9 @@ class HomeService extends Service {
   async user() {
     const { app } = this;
     const QUERY_STR = 'id, name';
-    const sql = `select ${QUERY_STR} from test_user`; // 获取 id 的 sql 语句
+    const sql = `select ${QUERY_STR} from test_user`; // the SQL of getting user list
     try {
-      const result = await app.mysql.query(sql); // mysql 实例已经挂载到 app 对象下，可以通过 app.mysql 获取到。
+      const result = await app.mysql.query(sql); // mysql has been loaded to the app object and can be obtained by app.mysql
       return result;
     } catch (error) {
       console.log(error);
