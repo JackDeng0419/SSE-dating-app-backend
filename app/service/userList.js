ed977dd3c7f89fa76f67f4e63136af750830260a
JackNeedFocus <Jack.z.deng@outlook.com>
2022-11-02 11:48:28 +1030

    merge with newest encryption 2

--- app/service/userList.js
+++ app/service/userList.js
@@ -1,28 +1,16 @@
 'use strict';
 
 const Service = require('egg').Service;
-<<<<<<< HEAD
 const LIKE_STATUS = require('../constant');
-=======
->>>>>>> wangwenjin
 
 class UserListService extends Service {
 
   async getUserListInSearchMate() {
-<<<<<<< HEAD
-=======
-    console.log('getUserListInSearchMate');
->>>>>>> wangwenjin
     const { app, ctx } = this;
     const requestUserID = ctx.session.user_info._uid;
     try {
 
-<<<<<<< HEAD
       const userList = await app.mysql.query(`SELECT picture as avatar, first_name as firstName, last_name as lastName, age, gender, city as location, _uid as userId FROM user LEFT JOIN user_profile on _uid=user_id WHERE NOT _uid=${requestUserID} `);
-=======
-      const userList = await app.mysql.query(`SELECT picture, first_name, last_name, age, gender, city FROM user LEFT JOIN user_profile on _uid=users_id WHERE NOT _uid=${requestUserID} `);
-      console.log('USERLIST', userList);
->>>>>>> wangwenjin
 
       return userList;
     } catch (error) {
@@ -36,12 +24,7 @@ class UserListService extends Service {
     const { app, ctx } = this;
     const requestUserID = ctx.session.user_info._uid;
     try {
-<<<<<<< HEAD
       const myLikesUsersID = await (await app.mysql.select('my_like', { where: { from_id: requestUserID, like_status: 1 } })).map(element => { return element.to_id; });
-=======
-      const myLikesUsersID = await app.mysql.select('my_like', { from_id: requestUserID });
-      console.log(myLikesUsersID);
->>>>>>> wangwenjin
       const myLikesUsers = await this.getUserListByIDArray(myLikesUsersID);
 
       return myLikesUsers;
@@ -52,7 +35,6 @@ class UserListService extends Service {
     }
   }
 
-<<<<<<< HEAD
   async getMyDislikesUsers() {
     const { app, ctx } = this;
     const requestUserID = ctx.session.user_info._uid;
@@ -110,15 +92,6 @@ class UserListService extends Service {
         age: element.age,
         userId: element.users_id,
         location: element.city }));
-=======
-  async getUserListByIDArray(id_array) {
-    const { app, ctx } = this;
-    const userList = [];
-    id_array.forEach(async userID => {
-      const user = await app.mysql.select('user_profile', { users_id: userID });
-      userList.push(user);
-    });
->>>>>>> wangwenjin
 
     return userList;
   }
